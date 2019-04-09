d6ab07b099deab1c3e3a8aaaef6e7c918bb17c79
teb.med <lamin.teb94@gmail.com>
2019-04-09 17:41:29 +0200

    Projet Creation

---
+++ GUI/src/main/java/fr/pantheonsorbonne/cri/GuiApplication.java
@@ -0,0 +1,13 @@
+package fr.pantheonsorbonne.cri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiApplication.class, args);
+	}
+
+}
